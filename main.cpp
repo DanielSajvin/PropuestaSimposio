54cc3c1f5c1b3e8b25eec9c40143b638562ea886
MarioDanielSajvin <msajvingomez@gmail.com>
2021-03-23 10:05:13 -0600

    Mario_Daniel_Sajvin_Gomez_1612921
    Programa que permite calcular en que salon de las ingenierias hay mas estudiantes.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+//Mario Daniel Sajvin Gomez 1612921
 int main()
 {
   std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
+
+
+  int sistem, indus, civil;
+
+  cout << "Ingrese la cantidad de estudiantes de la ingenieria en sistemas\n";
+  cin >> sistem; 
+  cout << endl;
+
+  cout << "Ingrese la cantidad de estudiantes de la ingenieria industrial\n";
+  cin >> indus;
+  cout << endl;
+
+  cout << "Ingrese la cantidad de estudiantes de la ingenieria civil\n";
+  cin >> civil;
+  cout << endl;
+
+  if (sistem > indus && sistem > civil)
+  {
+	  cout << "El salon con mayor cantidad de estudiantes es ingenieria en sistemas con: " << sistem << " estudiantes.\n";
+  }
+
+  else if (indus > sistem && indus > civil)
+  {
+	  cout << "El salon con mayor cantidad de estudiantes es ingenieria idustrial con: " << indus << " estudiantes." << endl;
+  }
+
+  else if (civil > sistem && civil > indus)
+  {
+	  cout << "El salon con mayor cantidad de estudiantes es ingenieria civil con: " << civil  << " estudiantes." << endl;
+  }
+
   std::cin.ignore();
   return 0;
 }
